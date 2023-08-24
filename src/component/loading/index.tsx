eadf37f56f063b0f159eda18a09e7d4185781ad4
zemo <15817289730@163.com>
2023-08-24 18:17:53 +0800

    feat: 修改loading样式

--- src/component/loading/index.tsx
+++ src/component/loading/index.tsx
@@ -15,13 +15,7 @@
 import { VoidComponent } from "solid-js";
 
 const Loding: VoidComponent = () => {
-  return (
-    <div class="klinecharts-pro-loading">
-      <i class="circle1" />
-      <i class="circle2" />
-      <i class="circle3" />
-    </div>
-  )
-}
+  return <div class="klinecharts-pro-loading" />;
+};
 
-export default Loding
+export default Loding;
